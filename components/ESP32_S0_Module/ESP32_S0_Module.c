429deb3fde7d585ddf8c5c9434df1675bf9361d8
SpenZerX <MSchulze780@gmail.com>
2018-10-10 13:38:08 +0200

    Update ESP32_S0_Module.c

--- components/ESP32_S0_Module/ESP32_S0_Module.c
+++ components/ESP32_S0_Module/ESP32_S0_Module.c
@@ -1,13 +1,17 @@
 ﻿/* #################################################################################################
  *
- *  Function: ESP32_S0 Module for SCDE (Smart Connected Device Engine)
+ *  ESP32_S0_Module for SCDE (Smart Connected Device Engine)
  *
- *  ESP 8266EX & ESP32 SOC Activities ...
- *  Copyright by EcSUHA
+ *  S0 -> S0 Feature.
+ *  It is used to analyze impulses at GPIOs (hi/lo level) with the goal to count them (S0 Power Meter,
+ *  S0 Gas meter, ...). The ESP32 SoC GPIO will be connected to the Counter-Hardware.
+ *  The Counter-Hardware is used and controlled by this Module. This is hardware counting.
  *
  *  Created by Maik Schulze, Sandfuhren 4, 38448 Wolfsburg, Germany for EcSUHA.de 
  *
  *  MSchulze780@GMAIL.COM
+ *  Copyright by EcSUHA
+ *
  *  EcSUHA - ECONOMIC SURVEILLANCE AND HOME AUTOMATION - WWW.EcSUHA.DE
  * #################################################################################################
  */
