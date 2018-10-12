865e0dd017bf76f7c4ffe72001d1549f76d6ad03
SpenZerX <MSchulze780@gmail.com>
2018-10-12 07:44:16 +0200

    adding 2 Commands

---
+++ components/Rereadcfg_Command/include/Rereadcfg_Command.h
@@ -0,0 +1,18 @@
+﻿// SCDE-Command Rereadcfg
+
+#ifndef REREADCFG_COMMAND_H
+#define REREADCFG_COMMAND_H
+
+
+
+#include "SCDE_s.h"
+
+
+
+int Rereadcfg_InitializeCommandFn(SCDERoot_t* SCDERootptr);
+
+struct headRetMsgMultiple_s Rereadcfg_CommandFn(const uint8_t *argsText, const size_t argsTextLen);
+
+
+
+#endif /*REREADCFG_COMMAND_H*/
---
+++ components/Save_Command/include/Save_Command.h
@@ -0,0 +1,18 @@
+﻿// SCDE-Command Save
+
+#ifndef SAVE_COMMAND_H
+#define SAVE_COMMAND_H
+
+
+
+#include "SCDE_s.h"
+
+
+
+int Save_InitializeCommandFn(SCDERoot_t* SCDERootptr);
+
+struct headRetMsgMultiple_s Save_CommandFn(const uint8_t *argsText, const size_t argsTextLen);
+
+
+
+#endif /*SAVE_COMMAND_H*/
