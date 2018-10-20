55ffa22c5c0d3c9783d6b0a37b651acdb3c38fcb
SpenZerX <MSchulze780@gmail.com>
2018-10-20 09:43:17 +0200

    adding files

---
+++ components/List_Command/include/List_Command.h
@@ -0,0 +1,22 @@
+﻿// Command List for SCDE
+
+#ifndef LIST_COMMAND_H
+#define LIST_COMMAND_H
+
+
+/*
+ *  SCDE Typedefs and Structures
+ */
+#include "SCDE_s.h"
+
+
+
+// the initialize Fn
+int List_InitializeCommandFn(SCDERoot_t* SCDERootptr);
+
+// the main command Fn
+struct headRetMsgMultiple_s List_CommandFn (const uint8_t *args, const size_t argsLen);
+
+
+
+#endif /*LIST_COMMAND_H*/
