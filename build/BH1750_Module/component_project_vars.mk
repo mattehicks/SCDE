e051eafa1bb5083679b264baaac93b9899dd0c39
SpenZerX <MSchulze780@gmail.com>
2019-02-21 20:23:51 +0100

    adding 21.02.2019

---
+++ build/BH1750_Module/component_project_vars.mk
@@ -0,0 +1,7 @@
+# Automatically generated build file. Do not edit.
+COMPONENT_INCLUDES += $(PROJECT_PATH)/components/BH1750_Module/include $(PROJECT_PATH)/main/include
+COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/BH1750_Module -lBH1750_Module
+COMPONENT_LINKER_DEPS += 
+COMPONENT_SUBMODULES += 
+COMPONENT_LIBRARIES += BH1750_Module
+component-BH1750_Module-build: 
